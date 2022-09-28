be521166dddcf3860c117801a11913393974889e
raheemadamboev <vividricemaryandmax@gmail.com>
2022-09-28 14:19:02 +0500

    material3 theme setup

--- app/src/main/java/xyz/teamgravity/customcalendarview/Main.kt
+++ app/src/main/java/xyz/teamgravity/customcalendarview/Main.kt
@@ -3,7 +3,7 @@ package xyz.teamgravity.customcalendarview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
