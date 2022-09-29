b3d5b881c33b407cacbe2087e3e9726b58d3cd5a
raheemadamboev <vividricemaryandmax@gmail.com>
2022-09-29 15:12:34 +0500

    provide fragment scoped dependencies

---
+++ app/src/main/java/xyz/teamgravity/customcalendarview/core/util/Helper.kt
@@ -0,0 +1,35 @@
+package xyz.teamgravity.customcalendarview.core.util
+
+import com.kizitonwose.calendarview.model.CalendarMonth
+import java.time.DayOfWeek
+import java.time.format.DateTimeFormatter
+import java.time.temporal.WeekFields
+import java.util.*
+
+object Helper {
+
+    /**
+     * Returns array of DayOfWeek according to locale order
+     * (MUN or SUN) first?
+     */
+    fun daysOfWeekFromLocale(): Array<DayOfWeek> {
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        val daysOfWeek = DayOfWeek.values()
+        // Order `daysOfWeek` array so that firstDayOfWeek is at index 0.
+        // Only necessary if firstDayOfWeek is not DayOfWeek.MONDAY which has ordinal 0.
+        if (firstDayOfWeek != DayOfWeek.MONDAY) {
+            val rhs = daysOfWeek.sliceArray(firstDayOfWeek.ordinal..daysOfWeek.indices.last)
+            val lhs = daysOfWeek.sliceArray(0 until firstDayOfWeek.ordinal)
+            return rhs + lhs
+        }
+        return daysOfWeek
+    }
+
+    /**
+     * Used for getting formatted string
+     */
+    private val yearMonthFormatter: DateTimeFormatter by lazy { DateTimeFormatter.ofPattern("MMMM") }
+    fun formatYearMonth(month: CalendarMonth): String {
+        return "${yearMonthFormatter.format(month.yearMonth)} ${month.yearMonth.year}"
+    }
+}
