ca9bfe8d0b9bc30e384136edcddb9acf42606342
raheemadamboev <vividricemaryandmax@gmail.com>
2022-09-29 16:04:00 +0500

    provide timber debug tree

--- app/src/main/java/xyz/teamgravity/customcalendarview/injection/provide/ApplicationModule.kt
+++ app/src/main/java/xyz/teamgravity/customcalendarview/injection/provide/ApplicationModule.kt
@@ -4,12 +4,17 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import timber.log.Timber
 import xyz.teamgravity.customcalendarview.data.repository.MainRepository
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideTimberDebugTree(): Timber.DebugTree = Timber.DebugTree()
 
     @Provides
     @Singleton
